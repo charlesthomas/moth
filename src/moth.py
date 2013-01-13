bcb1af4e7f210886b2f7f4a01c91ca0909cbc275
Charles Thomas <ch@rlesthom.as>
2013-01-13 18:06:44 -0500

    added ip-based auth and expiration support

--- src/moth.py
+++ src/moth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from string import ascii_letters, digits
 from random import choice
+from datetime import timedelta, datetime
 
 class mothException( Exception ): pass
 
@@ -8,34 +9,54 @@ class Moth( object ):
     def __init__( self, collection ):
         self.collection = collection
 
-    def create_token( self, address, token_size = 64 ):
+    def create_token( self, address, ip = None, expire = None, token_size = 64 ):
         token = ''.join(
             choice( ascii_letters + digits ) for x in range( token_size )
         )
 
-        if self.store_token( token, address ):
+        if self.store_token( token, address, ip, expire ):
             return token
 
         raise mothException( "failed to store token" )
 
-    def auth_token( self, token, address ):
+    def auth_token( self, token, address, ip = None ):
         user = self.fetch_user_data( address )
         if not user: return False
-        if token in user[ 'token' ]: return True
-        return False
+
+        if token not in user[ 'token' ]: return False
+
+        #over-complicated so if the stored token doesn't have an ip,
+        #passing one to auth won't return False
+        token_ip = user[ 'token' ][ token ].get( 'ip' )
+        if token_ip and ip != token_ip:
+            return False
+
+        token_expire = user[ 'token' ][ token ].get( 'expire' )
+        if token_expire and token_expire < datetime.now().strftime( '%s' ):
+            self.remove_token( token, address )
+            return False
+
+        return True
+
+    def store_token( self, token, address, ip = None, expire = None ):
+        payload = {}
+        if ip: payload[ 'ip' ] = ip
+        if expire:
+            expire = datetime.now() + timedelta( days = expire )
+            payload[ 'expire' ] = expire.strftime( '%s' )
         
-    def store_token( self, token, address ):
         existing = self.fetch_user_data( address )
         if existing:
             tokens = existing[ 'token' ]
-            tokens.append( token )
+            tokens[ token ] = payload
+
             return self.collection.update(
                 { '_id' : existing[ '_id' ] },
                 { "$set" : { 'token' : tokens } }
             )
 
         return self.collection.insert( {
-            'token' : [ token ],
+            'token' : { token : payload },
             'address' : address.lower(),
         } )
 
@@ -51,7 +72,7 @@ class Moth( object ):
             raise mothException( "token doesn't exist" )
 
         tokens = user[ 'token' ]
-        tokens.pop( tokens.index( token ) )
+        tokens.pop( token )
         return self.collection.update(
             { '_id' : user[ '_id' ] },
             { "$set" : { 'token' : tokens } }
