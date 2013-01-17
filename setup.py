255ea6cfe846d6b8c11c996c2c9fe0e9da83b57f
Charles Thomas <ch@rlesthom.as>
2013-01-17 17:17:00 -0500

    hopefully this will allow me to add this package to other pip_requirements now

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from distustils.core import setup
+from os.path import dirname, join
+
+VERSION = open( join( dirname( __file__ ), 'VERSION' ) ).read()
+DESCRIPTION = 'Authentication made easy'
+
+setup(
+    name = 'moth',
+    version = VERSION,
+    description = DESCRIPTION,
+    author = 'Charles Thomas',
+    author_email = 'ch@rlesthom.as',
+    url = 'http://bitbucket.org/charlesthomas/moth',
+)
