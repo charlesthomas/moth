45656d3ce9ec5bfab7f721bcb38cff4d54e06d62
Charles Thomas <ch@rlesthom.as>
2013-04-21 17:33:03 -0400

    added ret_val for allowing a custom field to be returned on auth, instead of just True

--- moth.py
+++ moth.py
@@ -3,77 +3,81 @@ from string import ascii_letters, digits
 from random import choice
 from datetime import timedelta, datetime
 
-class mothException( Exception ): pass
+class mothException(Exception): pass
 
-class Moth( object ):
-    def __init__( self, collection ):
+class Moth(object):
+    def __init__(self, collection):
         self.collection = collection
 
-    def create_token( self, email, ip = None, expire = None, token_size = 64 ):
+    def create_token(self, email, ip=None, expire=None, token_size=64,
+                     ret_val=None):
         token = ''.join(
-            choice( ascii_letters + digits ) for x in range( token_size )
+            choice(ascii_letters + digits) for x in range(token_size)
         )
 
-        if self.store_token( token, email, ip, expire ):
+        if self.store_token(token, email, ip, expire, ret_val):
             return token
 
-        raise mothException( "failed to store token" )
+        raise mothException("failed to store token")
 
-    def auth_token( self, token, email, ip = None ):
-        user = self.fetch_user_data( email )
+    def auth_token(self, token, email, ip=None):
+        user = self.fetch_user_data(email)
         if not user: return False
 
-        if token not in user[ 'token' ]: return False
+        if token not in user['token']: return False
 
         #over-complicated so if the stored token doesn't have an ip,
         #passing one to auth won't return False
-        token_ip = user[ 'token' ][ token ].get( 'ip' )
+        token_ip = user['token'][token].get('ip')
         if token_ip and ip != token_ip:
             return False
 
-        token_expire = user[ 'token' ][ token ].get( 'expire' )
-        if token_expire and token_expire < datetime.now().strftime( '%s' ):
-            self.remove_token( token, email )
+        token_expire = user['token'][token].get('expire')
+        if token_expire and token_expire < datetime.now().strftime('%s'):
+            self.remove_token(token, email)
             return False
 
+        ret_val = user['token'][token].get('ret_val')
+        if ret_val: return ret_val
         return True
 
-    def store_token( self, token, email, ip = None, expire = None ):
+    def store_token(self, token, email, ip=None, expire=None, ret_val=None):
         payload = {}
-        if ip: payload[ 'ip' ] = ip
+        if ip: payload['ip'] = ip
+        if ret_val: payload['ret_val'] = ret_val
         if expire:
-            expire = datetime.now() + timedelta( days = expire )
-            payload[ 'expire' ] = expire.strftime( '%s' )
+            expire = datetime.now() + timedelta(days = expire)
+            payload['expire'] = expire.strftime('%s')
         
-        existing = self.fetch_user_data( email )
+        existing = self.fetch_user_data(email)
         if existing:
-            tokens = existing[ 'token' ]
-            tokens[ token ] = payload
+            tokens = existing['token']
+            tokens[token] = payload
 
             return self.collection.update(
-                { '_id' : existing[ '_id' ] },
+                { '_id' : existing['_id'] },
                 { "$set" : { 'token' : tokens } }
             )
 
-        return self.collection.insert( {
+        return self.collection.insert({
             'token' : { token : payload },
             'email' : email.lower(),
-        } )
+        })
 
-    def fetch_user_data( self, email ):
-        return self.collection.find_one( { 'email' : email.lower() } )
+    def fetch_user_data(self, email):
+        return self.collection.find_one({ 'email' : email.lower() })
 
-    def remove_user( self, id ):
-        return self.collection.remove( id )
+    def remove_user(self, id):
+        return self.collection.remove(id)
 
-    def remove_token( self, token, email ):
-        user = self.fetch_user_data( email )
-        if not token in user[ 'token' ]:
-            raise mothException( "token doesn't exist" )
+    def remove_token(self, token, email):
+        user = self.fetch_user_data(email)
+        if not token in user['token']:
+            raise mothException("token doesn't exist")
 
-        tokens = user[ 'token' ]
-        tokens.pop( token )
+        tokens = user['token']
+        tokens.pop(token)
         return self.collection.update(
-            { '_id' : user[ '_id' ] },
+            { '_id' : user['_id'] },
             { "$set" : { 'token' : tokens } }
         )
