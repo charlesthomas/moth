c89f6b47ccf7acc2aeffb8629fc2063e73b5218a
Charles Thomas <ch@rlesthom.as>
2014-01-16 15:54:35 -0500

    changing url

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     author = 'Charles Thomas',
     author_email = 'ch@rlesthom.as',
     packages = ['%s' % NAME],
-    url = 'http://code.cha.rlesthom.as/%s' % NAME,
+    url = 'https://github.com/charlesthomas/%s' % NAME,
     license = 'MIT',
     description = DESCRIPTION,
     long_description = LONG_DESC,
